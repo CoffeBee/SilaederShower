ffa9c8c6a150aa2129c0163f6ebbf36f35b56146
Ivan Podvorniy <ivan.podvorniy@yandex.ru>
2022-10-20 20:54:15 +0400

    add basic section stuff

--- Public/home_scripts.js
+++ Public/home_scripts.js
@@ -4,10 +4,15 @@ function openNav() {
 	document.getElementById("contentInNav").style.visibility = "visible";
 	document.getElementById("contentInNav").style.padding = "30px 40px";
 	document.getElementById("logoInNav").style.visibility = "hidden";
+		document.getElementById("plan").style.width = "calc(100vw - 351px - 69px - 30px)";
+
 }
 function closeNav() {
 	document.getElementById("slidebar").style.width = "165px";
 	document.getElementById("app").style.marginLeft = "calc(184px + 69px)";
+	document.getElementById("plan").style.width = "calc(100vw - 184px - 69px - 30px)";
+
 	document.getElementById("contentInNav").style.visibility = "hidden";
 	document.getElementById("logoInNav").style.visibility = "visible";
+
 }
--- Sources/App/Controllers/ConferenceController.swift
+++ Sources/App/Controllers/ConferenceController.swift
@@ -7,6 +7,7 @@
 
 import Vapor
 import Fluent
+import Leaf
 
 
 
@@ -25,6 +26,10 @@ struct ChangeDetail: Content {
     let detail: String
 }
 
+struct ChangeName: Content {
+    let name: String
+}
+
 struct ConferenceController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let conferencesRoute = routes.grouped("cf")
@@ -34,6 +39,12 @@ struct ConferenceController: RouteCollection {
         conferencesRoute.get(":id", use: viewConf)
         conferencesRoute.post("title", ":id", use: titleChange)
         conferencesRoute.post("detail", ":id", use: detailChange)
+        conferencesRoute.post("section", ":id", use: sectionAdd)
+        conferencesRoute.get("section", ":id", use: sections)
+        
+        let sectionsRoute = conferencesRoute.grouped("sc")
+        
+        sectionsRoute.post("name", ":id", use: changeName)
     }
     
     
@@ -59,6 +70,24 @@ struct ConferenceController: RouteCollection {
         }.flatMap { $0 }
     }
     
+    fileprivate func getSectionsPublics(sections: [Section], firstIDOpt: Section.IDValue?) throws -> [Section.Public] {
+        guard var firstID = firstIDOpt else {
+            return []
+        }
+        
+        var sectionIDMap = [Section.IDValue: Section]()
+        for section in sections {
+            try sectionIDMap[section.requireID()] = section
+        }
+        var result = [Section.Public]()
+        while let nextID = sectionIDMap[firstID]?.$nextSection.id {
+            result.append(Section.Public(id: firstID, name: sectionIDMap[firstID]!.name))
+            firstID = nextID
+        }
+        result.append(Section.Public(id: firstID, name: sectionIDMap[firstID]!.name))
+        return result
+    }
+    
     fileprivate func viewConf(req: Request) throws -> EventLoopFuture<View> {
         let confStr = req.parameters.get("id")!
         guard let confID = UUID(uuidString: confStr) else {
@@ -66,7 +95,9 @@ struct ConferenceController: RouteCollection {
         }
         return Conference.find(confID, on: req.db).unwrap(or: Abort.redirect(to: "/cf")).flatMap { conf in
             return self.getConfs(req: req, selected: confID).flatMap { confs in
-                req.view.render("conf", ConfInformation(confs: confs, title: conf.title, detail: conf.detail, confID: confID))
+                return Section.query(on: req.db).filter(\.$conference.$id == confID).all().flatMapThrowing { sections in
+                    req.view.render("conf", ConfInformation(confs: confs, title: conf.title, detail: conf.detail, confID: confID))
+                }.flatMap { $0 }
             }
         }
     }
@@ -101,6 +132,54 @@ struct ConferenceController: RouteCollection {
             return req.redirect(to: "/cf")
         }
     }
+    
+    fileprivate func sectionAdd(req: Request) throws -> EventLoopFuture<Section.Public> {
+        let confStr = req.parameters.get("id")!
+        guard let confID = UUID(uuidString: confStr) else {
+            throw Abort(.badRequest)
+        }
+        return Conference.find(confID, on: req.db).unwrap(or: Abort(.notFound)).flatMap { conf in
+            return Section.query(on: req.db).filter(\.$conference.$id == confID).filter(\.$nextSection.$id == nil).first().flatMapThrowing { lastSectionOptional in
+                let newSection = try Section(conferenceID: confID)
+                return newSection.save(on: req.db).flatMapThrowing {
+                    Section.Public(id: try newSection.requireID(), name: newSection.name)
+                }.flatMap { sectionPublic in
+                    if let lastSection = lastSectionOptional {
+                        lastSection.$nextSection.id = sectionPublic.id
+                        return lastSection.save(on: req.db).map { sectionPublic }
+                    }
+                    conf.$firstSection.id = sectionPublic.id
+                    return conf.save(on: req.db).map { sectionPublic }
+                }
+            }.flatMap { $0 }
+        }
+        
+    }
+    
+    fileprivate func sections(req: Request) throws -> EventLoopFuture<[Section.Public]> {
+        let confStr = req.parameters.get("id")!
+        guard let confID = UUID(uuidString: confStr) else {
+            throw Abort(.badRequest)
+        }
+        return Conference.find(confID, on: req.db).unwrap(or: Abort(.notFound)).flatMap { conf in
+            return Section.query(on: req.db).filter(\.$conference.$id == confID).all().flatMapThrowing { sections in
+                try self.getSectionsPublics(sections: sections, firstIDOpt: conf.$firstSection.id)
+            }
+        }
+    }
+    
+    fileprivate func changeName(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        let sectionStr = req.parameters.get("id")!
+        guard let sectionID = UUID(uuidString: sectionStr) else {
+            throw Abort(.badRequest)
+        }
+        let name = try req.content.decode(ChangeName.self).name
+        return Section.find(sectionID, on: req.db).unwrap(or: Abort(.notFound)).flatMap { section in
+            section.name = name
+            return section.save(on: req.db).map { .ok }
+        }
+    }
+    
     
 }
 
