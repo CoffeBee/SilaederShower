a08d9716a16dbb8343329993b7db1162ccefbb79
Ivan Podvorniy <ivan.podvorniy@yandex.ru>
2022-10-24 01:14:53 +0400

    fix for mobile

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -9,6 +9,9 @@ import Vapor
 import Fluent
 import AsyncHTTPClient
 
+struct Pin: Content {
+    let code: String
+}
 
 struct UserController: RouteCollection {
     
@@ -25,7 +28,9 @@ struct UserController: RouteCollection {
                 UserCredentialsAuthenticator(),
             ])
         routes.get("login", use: loginGet)
+        routes.get("guest", use: guestGet)
         session.post("login", use: loginPost)
+        session.post("guest", use: guestPost)
         session.get { req -> Response in
             guard let _ = req.auth.get(User.self) else {
                 return req.redirect(to: "/login")
@@ -38,6 +43,26 @@ struct UserController: RouteCollection {
     
     fileprivate func loginGet(req: Request) throws -> EventLoopFuture<View> {
         return req.view.render("reg", ["error": false])
+    }
+    
+    fileprivate func guestGet(req: Request) throws -> EventLoopFuture<View> {
+        return req.view.render("part", ["error": false])
+    }
+    
+    fileprivate func guestPost(req: Request) throws -> EventLoopFuture<Response> {
+        let code = try req.content.decode(Pin.self).code
+        return Conference.query(on: req.db).filter(\.$secretHash == code).first().flatMap { confOpt in
+            guard let conf = confOpt else {
+                return req.view.render("part", ["error": false]).flatMapThrowing { $0.encodeResponse(for: req) }.flatMap { $0 }
+            }
+            return User.query(on: req.db).first().unwrap(or: Abort(.notFound)).flatMap { user  in
+                req.auth.login(user)
+                req.session.authenticate(user)
+                return req.eventLoop.makeSucceededFuture(req.redirect(to: "/cf/" + conf.id!.uuidString))
+            }
+            
+        }
+       
     }
    
     
